f7172eab05e730fefe396555b694b23a1a19c30b
Jair Balcazar <jairbalcazar.jb@gmail.com>
2020-10-23 14:26:26 -0500

    First commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GifExperApp from './GifExpertApp';
+import './index.css';
+
+ReactDOM.render(
+    <GifExperApp />,
+  document.getElementById('root')
+);
+
