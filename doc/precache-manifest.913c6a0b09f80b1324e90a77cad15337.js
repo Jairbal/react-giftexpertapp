6f48b4bcc805762d264679a99ae0ce37151ae1ea
Jair Balcazar <jairbalcazar.jb@gmail.com>
2020-10-23 14:27:36 -0500

    docs agregados

---
+++ doc/precache-manifest.913c6a0b09f80b1324e90a77cad15337.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b9d288de5b3cfe698b014741f4381f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2d748f21df8c3e2bce1",
+    "url": "/static/css/main.0a5a2069.chunk.css"
+  },
+  {
+    "revision": "50c5007e2135538c1738",
+    "url": "/static/js/2.a7fcd019.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a7fcd019.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2d748f21df8c3e2bce1",
+    "url": "/static/js/main.b1df273d.chunk.js"
+  },
+  {
+    "revision": "4e3a1f73b6e5de40b557",
+    "url": "/static/js/runtime-main.5c054f69.js"
+  }
+]);
